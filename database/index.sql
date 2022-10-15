9b6ce6f26f507a03253ec8d940eb490aecd71407
Guilherme G <98757828+GuilhermeGavioli@users.noreply.github.com>
2022-10-15 17:12:35 -0300

    corrige sql sintaxe

--- database/index.sql
+++ database/index.sql
@@ -1,20 +1,19 @@
-CREATE TABLE gender{
-    g_id CHAR(1) NOT NULL,
+CREATE TABLE gender(
+    g_id VARCHAR(255) NOT NULL UNIQUE,
     gender VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY(g_id)
-}
+);
 
 INSERT INTO gender VALUES ("1", "masculino"), ("2", "feminino");
 
-CREATE TABLE addr_state{
+CREATE TABLE addressState(
     id INT NOT NULL UNIQUE AUTO_INCREMENT,
     state_name VARCHAR(255) NOT NULL UNIQUE,
-    addr_state CHAR(2) NOT NULL UNIQUE,
+    state_initials VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY(id)
-}
+);
 
-INSERT INTO addr_state(addr_state) VALUES 
-( "Acre", "AC" ), (  "Alagoas", "AL" ), ( "Amapá, AP"), ( "Amazonas", "AM" ), ( "Bahia", "BA" ), ( "Ceará", "CE" ), 
+INSERT INTO addressState(state_name, state_initials) VALUES ( "Acre", "AC" ), (  "Alagoas", "AL" ), ( "Amapá", "AP"), ( "Amazonas", "AM" ), ( "Bahia", "BA" ), ( "Ceará", "CE" ), 
 ( "Distrito Federal", "DF" ), ( "Espírito Santo", "ES"), ( "Goiás", "GO"), ( "Maranhão", "MA" ), ( "Mato Grosso", "MT" ), 
 ( "Mato Grosso do Sul", "MS" ), ( "Minas Gerais", "MG"), ( "Pará", "PA" ), ( "Paraíba", "PB" ), ( "Paraná", "PR" ), 
 ( "Pernambuco", "PE"), ( "Piauí", "PI" ), ( "Rio de Janeiro", "RJ" ), ( "Rio Grande do Norte", "RN"), 
@@ -22,21 +21,21 @@ INSERT INTO addr_state(addr_state) VALUES
 ( "São Paulo", "SP" ), ( "Sergipe", "SE" ), ( "Tocantins", "TO" );
 
 
-CREATE TABLE user (
+CREATE TABLE user(
     userid  VARCHAR(255) NOT NULL UNIQUE,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    gender CHAR(1) NOT NULL,
+    gender VARCHAR(255) NOT NULL UNIQUE,
     addr_state INT NOT NULL UNIQUE,
     created_at DATE NOT NULL,
     updated_at DATE,
     PRIMARY KEY(userid),
-    FOREIGN KEY(addr_state) REFERENCES addr_state(id)
+    FOREIGN KEY(addr_state) REFERENCES addressState(id),
     FOREIGN KEY(gender) REFERENCES gender(g_id)
 );
 
-CREATE TABLE product (
+CREATE TABLE product(
     p_id  VARCHAR(255) NOT NULL UNIQUE,
     product_name VARCHAR(255) NOT NULL,
     userid VARCHAR(255) NOT NULL UNIQUE,
